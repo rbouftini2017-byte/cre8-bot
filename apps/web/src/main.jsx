130ca9dc630a61011eecd7a65dd9c1268b5f7bf0
rbouftini2017-byte <r.bouftini2017@gmail.com>
2025-10-25 01:07:14 +0100

    Update main.jsx

--- apps/web/src/main.jsx
+++ apps/web/src/main.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+
+import { HashRouter as BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import StoreCheckin from "./pages/StoreCheckin.jsx";
 
 const Home = () => (
